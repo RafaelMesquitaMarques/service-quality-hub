b1f89043bba9e0ff5dbb33d18f84dcbbbab0c854
RafaelMesquitaMarques <rmarques@foliot.com>
2026-06-05 07:24:26 -0400

    Update MobileLayout.jsx

--- client/src/pages/Mobile/MobileLayout.jsx
+++ client/src/pages/Mobile/MobileLayout.jsx
@@ -1,1 +1,15 @@
+import { useEffect } from 'react'
+import { useNavigate, Outlet } from 'react-router-dom'
+import { useAuthStore } from '../../store/authStore'
 
+export default function MobileLayout() {
+  const { user } = useAuthStore()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!user) navigate('/mobile/login', { replace: true })
+  }, [user])
+
+  if (!user) return null
+  return <Outlet />
+}
